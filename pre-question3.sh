00d1c3e1c11cce5199adc5d58baa2a7d33791dad
Jordi Camps <jcamps@gmail.com>
2025-01-14 13:01:08 +0100

    Upd: simplified some naming

--- pre-question3.sh
+++ pre-question3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker build -t dezc_python:1.0 .
+docker build -t dezc:1.0 .
 
 docker compose up -d
-docker run -v .:/app --network dezc dezc_python:1.0 python /app/ingest_data.py
+docker run --name dezc -v .:/app --network dezc dezc:1.0 python /app/ingest_data.py
 docker compose down
--- question3-4-5-6.sh
+++ question3-4-5-6.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --network dezc -v .:/app dezc_python:1.0 psql --tuples-only --no-align --quiet postgresql://root:root@db:5432/ny_taxi --file /app/question3-4-5-6.sql
+docker run --name dezc --network dezc -v .:/app dezc:1.0 psql --tuples-only --no-align --quiet postgresql://root:root@db:5432/ny_taxi --file /app/question3-4-5-6.sql
